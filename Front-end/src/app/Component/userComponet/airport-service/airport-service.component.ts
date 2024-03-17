10042d61fb493dbe827a174befd2d682f7350ad6
Kumar Mayur <kumarmayur@Kumars-MacBook-Pro.local>
2024-03-17 05:41:19 +0530

    frontend change is

---
+++ Front-end/src/app/Component/userComponet/airport-service/airport-service.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-airport-service',
+  templateUrl: './airport-service.component.html',
+  styleUrl: './airport-service.component.css'
+})
+export class AirportServiceComponent {
+
+}
